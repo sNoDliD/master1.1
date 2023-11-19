8eace4ddce42503b48d1b78829f50a6a2622558a
Dmytro Sukovanchenko <sukovanchenko.d@ajax.systems>
2023-11-19 19:58:14 +0200

    NN labs

--- neural_networks/README.md
+++ neural_networks/README.md
@@ -17,3 +17,7 @@ https://colab.research.google.com/drive/1qgP3PdfEmpAFtI1_wxfcT-5OsGKi9kgM?usp=sh
 ## Lec6
 
 https://colab.research.google.com/drive/11YK56Ft7-VZaB_f-hpnZNHnJOHGapSBJ?usp=sharing
+
+## Lec8+9
+
+https://colab.research.google.com/drive/19laYxiJllWizzf0XC9YSWJ3xFh5Ulcmn?usp=sharing
---
+++ neural_networks/lec8plus9/font_transform.py
@@ -0,0 +1,29 @@
+import os
+
+import pandas as pd
+
+
+def find_fonts():
+    for filename in os.listdir('fonts'):
+        df = pd.read_csv(f"fonts/{filename}")
+        if len(df) > 5_000:
+            print(filename.strip('.csv'), len(df))
+
+
+def combine_fonts():
+    result_data = []
+
+    fonts = ['ARIAL', 'TIMES', 'CALIBRI', 'MONEY', 'NUMERICS']
+    for font in fonts:
+        df = pd.read_csv(f"fonts/{font}.csv")
+        df_selected = df.iloc[:, 12:412]
+        df_selected['font'] = font
+        result_data.append(df_selected)
+    final_df = pd.concat(result_data, ignore_index=True)
+    shuffled = final_df.sample(frac=1, random_state=42)
+    return shuffled[['font'] + [col for col in shuffled.columns if col != 'font']]
+
+
+# find_fonts()
+df = combine_fonts()
+df.to_csv('fonts.csv', index=False)
